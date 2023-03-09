cd993eeb82f950943cc819c32dfb79f7e61439d5
Acemany <116539353+acemany@users.noreply.github.com>
2023-03-09 16:46:13 +0300

    Delete classes directory

--- classes/collides.py
+++
@@ -1,8 +0,0 @@
-class collided:
-    def circles(x1,y1,r1,x2,y2,r2)->bool:
-        return ((x2-x1)**2+(y2-y1)**2)**0.5<r1+r2
-    def boxes(x1,y1,w1,h1,x2,y2,w2,h2)->bool:
-        return (x1+w1>x2-w2 and
-                x1-w1<x2+w2 and
-                y1+h1>y2-h2 and
-                y1-h1<y2+h2)
